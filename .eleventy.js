434658f0a3567a5cedb64d3d1964228fa41771c3
djdanilo <djdanilo933@gmail.com>
2026-02-23 19:10:11 +0100

    Production ready

---
+++ .eleventy.js
@@ -0,0 +1,49 @@
+module.exports = function(eleventyConfig) {
+
+  // Static folders
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+
+  // Posts collection
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/posts/*.md");
+  });
+
+  // Date formatter
+  eleventyConfig.addFilter("formatDate", function(dateObj) {
+    const date = new Date(dateObj);
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}.`;
+  });
+
+  // Absolute URL filter
+  eleventyConfig.addFilter("absoluteUrl", function(url) {
+    return `https://uciteljicabranka.rs${url}`;
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes"
+    }
+  };
+  eleventyConfig.addCollection("categories", function(collectionApi) {
+  const posts = collectionApi.getFilteredByGlob("src/posts/*.md");
+  const categoryMap = {};
+
+  posts.forEach(post => {
+    const category = post.data.category;
+    if (!categoryMap[category]) {
+      categoryMap[category] = [];
+    }
+    categoryMap[category].push(post);
+  });
+
+  return categoryMap;
+});
+};
---
+++ src/posts/test.md
@@ -0,0 +1,8 @@
+---
+layout: post.njk
+title: "Test objava"
+date: 2026-02-23
+category: "I razred"
+featured_image: "/assets/photo1.svg"
+permalink: "/posts/test/"
+---
