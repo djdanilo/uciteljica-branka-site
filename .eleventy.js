6b3b51ffc150ae82decd6a1d006d7bc76ad9acda
djdanilo <djdanilo933@gmail.com>
2026-02-28 19:06:43 +0100

    CMS working version

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,20 @@
 module.exports = function(eleventyConfig) {
 
-  // Static folders
   eleventyConfig.addPassthroughCopy({ "src/admin": "admin" });
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
   eleventyConfig.addPassthroughCopy({ "src/js": "js" });
 
-  // Posts collection
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/*.md");
   });
 
-  // Date formatter
   eleventyConfig.addFilter("formatDate", function(dateObj) {
     const date = new Date(dateObj);
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
     return `${day}.${month}.${year}.`;
-  });
-
-  // Absolute URL filter
-  eleventyConfig.addFilter("absoluteUrl", function(url) {
-    return `https://uciteljicabranka.rs${url}`;
   });
 
   return {
@@ -32,18 +24,6 @@ module.exports = function(eleventyConfig) {
       includes: "_includes"
     }
   };
-  eleventyConfig.addCollection("categories", function(collectionApi) {
-  const posts = collectionApi.getFilteredByGlob("src/posts/*.md");
-  const categoryMap = {};
-
-  posts.forEach(post => {
-    const category = post.data.category;
-    if (!categoryMap[category]) {
-      categoryMap[category] = [];
-    }
-    categoryMap[category].push(post);
-  });
-
-  return categoryMap;
-});
+  
+  eleventyConfig.addPassthroughCopy({ "src/admin/decap-cms.js": "admin/decap-cms.js" });
 };
